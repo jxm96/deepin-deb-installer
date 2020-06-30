b36072e1e67d79143d1875251c905a309ca0b7c8
cuizhen <cuizhen@uniontech.com>
2020-06-30 18:20:48 +0800

    build: 修改CMAKE
    Description:
    修改为cmake
    
    Log:
    修改为cmake
    
    link:
    修改为cmake

--- src/deb_installer/translate_ts2desktop.sh
+++ src/deb_installer/translate_ts2desktop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DESKTOP_TEMP_FILE=deepin-deb-installer.desktop.tmp
-DESKTOP_SOURCE_FILE=deepin-deb-installer.desktop
-DESKTOP_DEST_FILE=deepin-deb-installer.desktop
+DESKTOP_TEMP_FILE=assets/deepin-deb-installer.desktop.tmp
+DESKTOP_SOURCE_FILE=assets/deepin-deb-installer.desktop
+DESKTOP_DEST_FILE=assets/deepin-deb-installer.desktop
 DESKTOP_TS_DIR=translations/desktop/
 
 /usr/bin/deepin-desktop-ts-convert ts2desktop $DESKTOP_SOURCE_FILE $DESKTOP_TS_DIR $DESKTOP_TEMP_FILE
