ddaacfae7061f84ab18a41cb0e782334aa9df051
cuizhen <cuizhen@uniontech.com>
2020-08-21 23:10:05 +0800

    fix: 空格
    空格
    
    Log:空格
    
    link: 空格

--- src/worker/aptworker.cpp
+++ src/worker/aptworker.cpp
@@ -656,12 +656,10 @@ void AptWorker::installFile()
     }
 
     m_dpkgProcess = new QProcess(this);
-    QString program = QLatin1String("dpkg") %
-            QLatin1String(" -i ") % '"' % m_trans->filePath() % '"';
     setenv("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", 1);
     setenv("DEBIAN_FRONTEND", "passthrough", 1);
     setenv("DEBCONF_PIPE", "/tmp/qapt-sock", 1);
-    m_dpkgProcess->start(program);
+    m_dpkgProcess->start("dpkg", QStringList() << "-i" << m_trans->filePath().replace(" ", "\\ "));
     connect(m_dpkgProcess, SIGNAL(started()), this, SLOT(dpkgStarted()));
     connect(m_dpkgProcess, SIGNAL(readyRead()), this, SLOT(updateDpkgProgress()));
     connect(m_dpkgProcess, SIGNAL(finished(int,QProcess::ExitStatus)),
